2d16aa60ef78020adb6b6a1dd19aa99defcc4b42
unknown <oguzhandemiraslan@gmail.com>
2022-11-03 15:00:08 -0400

    basic improvements

--- components/input.js
+++ components/input.js
@@ -3,30 +3,30 @@ import classes from "./input.module.css";
 
 function Input(props) {
   const [enteredInput, setEnteredInput] = useState("");
-  const [isValid, setIsValid] = useState(true);
 
   function inputChangeHandler(event) {
-    if (event.target.value.trim().lenght > 0) {
-      setIsValid(true);
-    }
     setEnteredInput(event.target.value);
   }
 
   function submitHandler(event) {
     event.preventDefault();
     if (enteredInput.trim().length === 0) {
-      setIsValid(false);
       return;
     }
     props.onAddTask(enteredInput);
   }
 
-  console.log(enteredInput);
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <div>
         <label htmlFor="input">To Do</label>
-        <input type="text" id="input" onChange={inputChangeHandler}></input>
+        <br></br>
+        <input
+          type="text"
+          id="input"
+          onChange={inputChangeHandler}
+          placeholder="Add something"
+        ></input>
       </div>
       <button type="submit">Save Task</button>
     </form>
--- components/task.js
+++ components/task.js
@@ -1,9 +1,15 @@
+import classes from "./task.module.css";
+
 function Task(props) {
   function deleteHandler() {
     props.onDelete(props.id);
   }
 
-  return <li onClick={deleteHandler}>{props.children}</li>;
+  return (
+    <li onClick={deleteHandler} className={classes.task}>
+      {props.children}
+    </li>
+  );
 }
 
 export default Task;
